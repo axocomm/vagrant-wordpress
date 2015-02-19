60d0a528ed1b93ee123c3777ab5b36483e88a3d3
axocomm <axocomm@gmail.com>
2015-02-19 14:11:01 -0500

    Fix a few issues in WordPress manifest

--- puppet/modules/wordpress/manifests/init.pp
+++ puppet/modules/wordpress/manifests/init.pp
@@ -2,7 +2,8 @@ class wordpress::wp-cli {
   exec {'download-wp-cli':
     path    => '/bin:/usr/bin',
     unless  => 'test -f /tmp/wp-cli.phar',
-    command => 'curl https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar > /tmp/wp-cli.phar'
+    command => 'curl https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar > /tmp/wp-cli.phar',
+    require => Package['php5-fpm']
   }
 
   exec {'install-wp-cli':
@@ -17,13 +18,14 @@ class wordpress::setup {
   exec {'download-wp-core':
     path    => '/usr/bin:/usr/local/bin',
     cwd     => '/vagrant',
+    user    => 'vagrant',
     unless  => 'test -f wp-load.php',
     command => 'wp core download'
   }
 
   exec {'create-wordpress-db':
     path    => '/usr/bin',
-    unless  => "test $(echo \"SELECT SCHEMA_NAME FROM INFORMATION_SCHEMA.SCHEMATA WHERE SCHEMA_NAME = '${::mysql_wordpress_dbname}'\" | wc -l) -gt 0",
+    unless  => "test $(echo \"SELECT SCHEMA_NAME FROM INFORMATION_SCHEMA.SCHEMATA WHERE SCHEMA_NAME = '${::mysql_wordpress_dbname}'\" | wc -l) -gt 1",
     command => "mysql -uroot -p${::mysql_root_password} -e 'CREATE SCHEMA ${::mysql_wordpress_dbname}'",
     require => Exec['set-mysql-root-password']
   }
@@ -40,6 +42,7 @@ class wordpress::setup {
     cwd => '/vagrant',
     unless => 'test -f wp-config.php',
     command => "wp core config --dbname=${::mysql_wordpress_dbname} --dbuser=${::mysql_wordpress_user} --dbpass=${::mysql_wordpress_password}",
+    user => 'vagrant',
     require => Exec['create-wordpress-user']
   }
 }
