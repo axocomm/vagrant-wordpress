290687e3f45b3ad7a49ec2c61d7c00ecb7ce2c75
axocomm <axocomm@gmail.com>
2016-02-02 13:41:33 -0500

    Add mariadb-server to phpmyadmin package dependencies and run bundled create_tables.sql

--- puppet/modules/phpmyadmin/manifests/init.pp
+++ puppet/modules/phpmyadmin/manifests/init.pp
@@ -1,12 +1,28 @@
 class phpmyadmin {
   package { 'phpmyadmin':
     ensure  => installed,
-    require => Package['php5-fpm'],
+    require => [Package['php5-fpm'],
+                Package['mariadb-server-10.0']],
   }
 
   file { '/www/phpmyadmin':
     ensure  => 'link',
     target  => '/usr/share/phpmyadmin',
     require => Package['phpmyadmin'],
+  }
+
+  file { '/etc/phpmyadmin/config-db.php':
+    source  => 'puppet:///modules/phpmyadmin/config-db.php',
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0640',
+    require => Package['phpmyadmin'],
+  }
+
+  exec { 'create-pma-tables':
+    command => "zcat /usr/share/doc/phpmyadmin/examples/create_tables.sql.gz | mysql -uroot -p${::mysql_root_password}",
+    unless  => 'test -d /var/lib/mysql/phpmyadmin',
+    require => Package['phpmyadmin'],
+    path    => '/bin:/usr/bin',
   }
 }
